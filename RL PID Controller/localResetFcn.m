c05293915deded3335e33a2239df1ccdfd388d82
AmirhosseinAsadi1380 <153992265+AmirhosseinAsadi1380@users.noreply.github.com>
2023-12-16 20:46:53 +0330

    Update localResetFcn.m

--- RL PID Controller/localResetFcn.m
+++ RL PID Controller/localResetFcn.m
@@ -11,13 +11,4 @@ while h <= 0.45 || h >= 0.6
 end
 in = setBlockParameter(in,blk,'Value',num2str(h));
 
-% % Randomize initial teta
-% h = 0.5*randn ;
-% while h <= 0 || h >= 2
-%     h = 0.5*randn ;
-% end
-% % blk = 'R_test/plant2/Integrator1';
-% blk = 'RL_R_flexible_mode1/Water-Tank System1/Integrator1';
-% in = setBlockParameter(in,blk,'InitialCondition',num2str(h));
-
 end
